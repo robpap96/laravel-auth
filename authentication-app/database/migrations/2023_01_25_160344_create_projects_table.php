85d253a824fc4203801c681f71168829772b64c1
robpap96 <robertopapagni1996@outlook.it>
2023-01-25 17:40:12 +0100

    aggiunta migrate

--- authentication-app/database/migrations/2023_01_25_160344_create_projects_table.php
+++ authentication-app/database/migrations/2023_01_25_160344_create_projects_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100)->unique();
+            $table->text('content');
+            $table->string('slug');
+            $table->date('created_on');
+            $table->boolean('private');
             $table->timestamps();
         });
     }
