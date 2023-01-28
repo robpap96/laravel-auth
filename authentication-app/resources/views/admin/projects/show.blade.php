ee9589c39cff3b2578a34a1dfeb200b4458cecd2
robpap96 <robertopapagni1996@outlook.it>
2023-01-28 17:41:22 +0100

    aggiunta immagine in show

--- authentication-app/resources/views/admin/projects/show.blade.php
+++ authentication-app/resources/views/admin/projects/show.blade.php
@@ -4,6 +4,8 @@
     <div class="container">
         <h2><a href="{{route('admin.projects.index')}}">Torna alla lista</a></h2>
         <h1>{{$project->name}}</h1>
-        <img src="{{$project->cover_image}}" alt="{{$project->name}}" width="100px">
+        @if ($project->cover_image)
+            <img src="{{asset("storage/$project->cover_image")}}" alt="{{$project->name}}" width="100px">
+        @endif
     </div>
 @endsection
